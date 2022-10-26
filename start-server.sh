bac79314c12e51b9bcef443e91798d2c3a29b8ab
JSteinbach <jsteinbach@mailbox.org>
2022-10-26 16:18:09 +0200

    Add setup-user.sh script

--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo Starting the CSS!
-
 cd ./CommunitySolidServer
 # specify port?
-npm start -- -c ./config/file-no-setup.json -f ../css-data/ -p 3000
+npm start -- -c ./config/file-no-setup.json -f ../css-data/ -p 3000 &
+cd ..
